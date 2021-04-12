b9e41f9022500ab2799ba415bb2449859e52822a
cray-mx <md.rayaan.h@gmail.com>
2021-04-12 17:38:25 +0530

    Updated

--- src/components/home.js
+++ src/components/home.js
@@ -5,13 +5,22 @@ import sportsIcon from "../images/sports.svg";
 import historyIcon from "../images/history-book.svg";
 import geographyIcon from "../images/earth-globe.svg";
 import animalIcon from "../images/lion.svg";
-
+import politicsIcon from "../images/speech.svg";
+import mathIcon from "../images/math.svg";
+import vehicleIcon from "../images/sport-car.svg";
+import gameIcon from "../images/game-controller.svg";
+import scienceIcon from "../images/atom.svg";
+import generalIcon from "../images/thought.svg";
 import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <Fragment>
       <div className="categories">
+        <Link className="card" to="/gk">
+          <img src={generalIcon} alt="" />
+          <p>General Knowledge</p>
+        </Link>
         <Link className="card" to="/cs">
           <img src={codingIcon} alt="" />
           <p>Computer Science</p>
@@ -35,6 +44,26 @@ const Home = () => {
         <Link className="card" to="/sports">
           <img src={sportsIcon} alt="" />
           <p>Sports</p>
+        </Link>
+        <Link className="card" to="/politics">
+          <img src={politicsIcon} alt="" />
+          <p>Politics</p>
+        </Link>
+        <Link className="card" to="/math">
+          <img src={mathIcon} alt="" />
+          <p>Mathematics</p>
+        </Link>
+        <Link className="card" to="/vehicles">
+          <img src={vehicleIcon} alt="" />
+          <p>Vehicles</p>
+        </Link>
+        <Link className="card" to="/games">
+          <img src={gameIcon} alt="" />
+          <p>Video Games</p>
+        </Link>
+        <Link className="card" to="/science">
+          <img src={scienceIcon} alt="" />
+          <p>Science</p>
         </Link>
       </div>
     </Fragment>
